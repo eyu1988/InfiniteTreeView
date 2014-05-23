31e7f41a2f806fbc806ee4eef81ac4abc48ae497
eyu1988 <lucas@CBDWSS.local>
2014-05-23 19:45:39 +0800

    1.0

---
+++ InfiniteTreeView/PushTree/ITTXibView.h
@@ -0,0 +1,15 @@
+//
+//  ITTXibView.h
+//  Sword
+//
+//  Created by Sword on 3/9/12.
+//  Copyright (c) 2012 iTotemStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ITTXibView : UIView
+
++ (id)loadFromXib;
+
+@end
